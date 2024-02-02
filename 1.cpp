8d3a519e4e2a50d76239ec6115f96409e011a8b1
Priyanshi Paghadal <paghadalpriyanshi097@gmail.com>
2024-02-02 20:58:25 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no1,no2,ans;
+    cout<<"Enter No 1 :"<<endl;
+    cin>>no1;
+    cout<<"Enter No 2 :"<<endl;
+    cin>>no2;
+    ans=no1+no2;
+    cout<<no1<<"+"<<no2<<"="<<ans;
+    return 0;
+}
